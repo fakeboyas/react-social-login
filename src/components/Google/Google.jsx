dcd082917d551e56d9024dc1467d9c9d0fb54aad
fakeboyas <ovoivi9@gmail.com>
2020-07-09 13:56:03 +0700

    try one

--- src/components/Google/Google.jsx
+++ src/components/Google/Google.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, {useState}from 'react'
 import { GoogleLogin } from "react-google-login";
 import { useHistory } from "react-router-dom";
 
 function Google() {
+    const [google,setGoogle] = useState({
+        name:"",
+        email:"",
+        img:""
+,    })
+    
     const history = useHistory();
     const responseGoogle = (response) => {
         if (response !== null) {
-            history.push("/home");
+            // history.push("/home");
+            console.log(response)
+            setGoogle({...google,name:response.profileObj.name})
+            setGoogle({...google,email:response.profileObj.email})
+            setGoogle({...google,email:response.profileObj.imageUrl})
+            localStorage.setItem('users',google)
         }
+        
     }
 
 
